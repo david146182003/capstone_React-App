2ea52c8713b98e5419a304d59f4aabf866349985
david <david146182003@gmail.com>
2025-07-16 11:10:32 -0400

    installed dotenv

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function connectDb(){
     try {
-        await mongoose.connect("mongodb+srv://david146182003:1124@cluster0.qlizzkg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        await mongoose.connect(process.env.MONGO_URL)
         console.log('MongoDB connected')
     }catch(e){
         console.log(e)
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import cors from 'cors'
 import connectDb from './db.js';
+import 'dotenv/config'
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT;
 
 app.use(cors())
 
